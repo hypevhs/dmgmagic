b0eee4626cd54282e2b185b586dffd87f58f18a2
hypershadsy <hypershadsy@gmail.com>
2014-08-02 18:46:15 -0400

    Start splitting up into files Prepare for precalculated-parallax

---
+++ algorithms.asm
@@ -0,0 +1,81 @@
+Mul8b:					; this routine performs the operation HL=H*E
+	ld d, 0				; clearing D and L
+	ld l, d
+	ld b, 8				; we have 8 bits
+Mul8bLoop:
+	add hl, hl			; advancing a bit
+	jp nc, Mul8bSkip	; if zero, we skip the addition (jp is used for speed)
+	add hl, de			; adding to the product if necessary
+Mul8bSkip:
+	dec b
+	jp nz, Mul8bLoop
+	ret
+
+Mul16:					; This routine performs the operation DEHL=BC*DE
+	ld hl, 0
+	ld a, 16
+Mul16Loop:
+	add hl, hl
+	rl e
+	rl d
+	jp nc, NoMul16
+	add hl, bc
+	jp nc, NoMul16
+	inc de				; This instruction (with the jump) is like an "ADC DE,0"
+NoMul16:
+	dec a
+	jp nz, Mul16Loop
+	ret
+
+;Div8:				; this routine performs the operation HL=HL/D
+;	xor a			; clearing the upper 8 bits of AHL
+;	ld b,16			; the length of the dividend (16 bits)
+;Div8Loop:
+;	add hl,hl		; advancing a bit
+;	rla
+;	cp d			; checking if the divisor divides the digits chosen (in A)
+;	jp c,Div8NextBit; if not, advancing without subtraction
+;	sub d			; subtracting the divisor
+;	inc l			; and setting the next digit of the quotient
+;Div8NextBit:
+;	djnz Div8Loop
+;	ret
+
+; GetKeys: adapted from APOCNOW.ASM and gbspec.txt
+GetKeys:			; gets keypress
+	ld a, P1F_5		; set bit 5
+	ld [rP1], a		; select P14 by setting it low
+	ld a, [rP1]
+ 	ld a, [rP1]		; wait a few cycles
+	cpl				; complement A. "You are a very very nice Accumulator..."
+	and $0f			; look at only the first 4 bits
+	swap a			; move bits 3-0 into 7-4
+	ld b,a			; and store in b
+
+ 	ld a, P1F_4		; select P15
+ 	ld [rP1], a
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]
+	ld a, [rP1]		; wait for the bouncing to stop
+	cpl				; as before, complement...
+	and $0f			; and look only for the last 4 bits
+	or b			; combine with the previous result
+	ret				; do we need to reset joypad? (gbspec line 1082)
+
+; StopLCD:
+; turn off LCD if it is on and wait until the LCD is off
+StopLCD:
+	ld a,[rLCDC]
+	rlca			; Put the high bit of LCDC into the Carry flag
+	ret nc			; Screen is off already. Exit.
+.wait:				; Loop until we are in VBlank
+	ld a, [rLY]
+	cp 145			; Is display on scan line 145 yet?
+	jr nz, .wait	; no, keep waiting
+	ld a, [rLCDC]	; Turn off the LCD
+	res 7, a		; Reset bit 7 of LCDC
+	ld [rLCDC], a
+	ret
--- hello-sprite.asm
+++ hello-sprite.asm
@@ -10,14 +10,21 @@ INCLUDE "gbhw.inc" ; standard hardware definitions from devrs.com
 INCLUDE "ibmpc1.inc" ; ASCII character set from devrs.com
 INCLUDE "sprite.inc" ; specific defs
 
-LayerGrassStart EQU $0a * 8
-LayerTrackStart EQU $0c * 8
+LoNVar:	MACRO
+\1		EQU	LoRamBase
+LoRamBase	SET	LoRamBase+(\2)
+		ENDM
+
+PLXStart EQU $0a * 8
+PLXEnd EQU $0f * 8
+PLXOffset EQU 75
 ScreenHeight EQU $12 * 8
 
 ; create variables. make sure to use tab (why??)
 	SpriteAttr Sprite0
 	LoByteVar VBLANKED
 	LoWordVar scrollX
+	LoNVar asdf, 10
 
 ; IRQs
 SECTION "Vblank", HOME[$0040]
@@ -217,9 +224,9 @@ MainLoop:
 	srl b
 	rr c
 	srl b
-	rr c				; mountains move at 1/4 pixels per second
+	rr c
 	ld a, c
-	ld [rSCX], a
+	ld [rSCX], a		; mountains move at 1/4 pixels per second
 	
 	call	GetKeys
 	
@@ -322,36 +329,36 @@ LCDC_STAT:
 	push bc
 	
 	ld a, [rLY]			; read scanline
-	ld h, 80-1			; -1 because I'm using way too many clock cycles
+	ld h, PLXStart-1	; -1 because I'm using way too many clock cycles
 	cp h
-	jp C, endLCDC		; if scanLine < 80, go to the end
+	jp C, endLCDC		; if scanLine < LayerGrassStart, go to the end
 	
-	ld h, 122
+	ld h, PLXEnd
 	cp h
-	jp NC, endLCDC		; if scanline >= 122, go to the end
+	jp NC, endLCDC		; if scanline >= LayerTrackStart, go to the end
 	
 	ld hl, 0			; clear hl dunno...
 	
-	sub 75
-	ld h, a				; h = scanline - 56
+	sub PLXOffset
+	ld h, a				; h = scanline - 75
 	
 	ld a, [scrollX]
 	ld e, a
 	ld a, [scrollX+1]
 	ld d, a				; de = scrollX 16bit
 	
-	ld a, h				; a = (scanline - 56)
-	sra a				; a = (scanline - 56) >> 1
+	ld a, h				; a = (scanline - 75)
+	sra a				; a = (scanline - 75) >> 1
 	ld c, a				; h = ^^
 	ld b, 0
-	call Mul16			; hl = (scrollX * (scanline - 56) >> 1)
+	call Mul16			; hl = (scrollX * (scanline - 75) >> 1)
 	
 	srl h
 	rr l
 	srl h
 	rr l
 	srl h
-	rr l				; hl = (scrollX * (scanline - 56) >> 1) >> 3
+	rr l				; hl = (scrollX * (scanline - 75) >> 1) >> 3
 	
 	ld a, l				; a = low order bits
 	
@@ -364,84 +371,4 @@ endLCDC:
 	pop af
 	reti
 
-Mul8b:					; this routine performs the operation HL=H*E
-	ld d, 0				; clearing D and L
-	ld l, d
-	ld b, 8				; we have 8 bits
-Mul8bLoop:
-	add hl, hl			; advancing a bit
-	jp nc, Mul8bSkip	; if zero, we skip the addition (jp is used for speed)
-	add hl, de			; adding to the product if necessary
-Mul8bSkip:
-	dec b
-	jp nz, Mul8bLoop
-	ret
-
-Mul16:					; This routine performs the operation DEHL=BC*DE
-	ld hl, 0
-	ld a, 16
-Mul16Loop:
-	add hl, hl
-	rl e
-	rl d
-	jp nc, NoMul16
-	add hl, bc
-	jp nc, NoMul16
-	inc de				; This instruction (with the jump) is like an "ADC DE,0"
-NoMul16:
-	dec a
-	jp nz, Mul16Loop
-	ret
-
-;Div8:				; this routine performs the operation HL=HL/D
-;	xor a			; clearing the upper 8 bits of AHL
-;	ld b,16			; the length of the dividend (16 bits)
-;Div8Loop:
-;	add hl,hl		; advancing a bit
-;	rla
-;	cp d			; checking if the divisor divides the digits chosen (in A)
-;	jp c,Div8NextBit; if not, advancing without subtraction
-;	sub d			; subtracting the divisor
-;	inc l			; and setting the next digit of the quotient
-;Div8NextBit:
-;	djnz Div8Loop
-;	ret
-
-; GetKeys: adapted from APOCNOW.ASM and gbspec.txt
-GetKeys:			; gets keypress
-	ld a, P1F_5		; set bit 5
-	ld [rP1], a		; select P14 by setting it low
-	ld a, [rP1]
- 	ld a, [rP1]		; wait a few cycles
-	cpl				; complement A. "You are a very very nice Accumulator..."
-	and $0f			; look at only the first 4 bits
-	swap a			; move bits 3-0 into 7-4
-	ld b,a			; and store in b
-
- 	ld a, P1F_4		; select P15
- 	ld [rP1], a
-	ld a, [rP1]
-	ld a, [rP1]
-	ld a, [rP1]
-	ld a, [rP1]
-	ld a, [rP1]
-	ld a, [rP1]		; wait for the bouncing to stop
-	cpl				; as before, complement...
-	and $0f			; and look only for the last 4 bits
-	or b			; combine with the previous result
-	ret				; do we need to reset joypad? (gbspec line 1082)
-
-; StopLCD:
-; turn off LCD if it is on and wait until the LCD is off
-StopLCD:
-	ld a,[rLCDC]
-	rlca			; Put the high bit of LCDC into the Carry flag
-	ret nc			; Screen is off already. Exit.
-.wait:				; Loop until we are in VBlank
-	ld a, [rLY]
-	cp 145			; Is display on scan line 145 yet?
-	jr nz, .wait	; no, keep waiting
-	ld a, [rLCDC]	; Turn off the LCD
-	res 7, a		; Reset bit 7 of LCDC
-	ld [rLCDC], a
-	ret
+INCLUDE "algorithms.asm"
